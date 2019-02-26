996b4932fdf46cfbee3c588d24136bc83034b287
Igor Lazarev <strider2038@yandex.ru>
2019-02-26 21:02:56 +0300

    base interfaces

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# json-rpc-client
+# JSON RPC v2 client for PHP
+
 Tiny JSON RPC v2 client for PHP
+
+## Roadmap
+
+* [ ] base client functions
+  * [ ] single request
+  * [ ] batch request
+* [ ] serializer interface
+* [ ] add tcp transport support
+* [ ] add http transport support via guzzle
+* [ ] bridge for symfony serializer
+* [ ] bridge for jms serializer
+* [ ] middleware interfaces
---
+++ src/BatchRequestInterface.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * This file is part of JSON RPC Client.
+ *
+ * (c) Igor Lazarev <strider2038@yandex.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Strider2038\JsonRpcClient;
+
+/**
+ * @author Igor Lazarev <strider2038@yandex.ru>
+ */
+interface BatchRequestInterface extends RequestInterface
+{
+    public function call(string $method, $params): BatchRequestInterface;
+    public function notify(string $method, $params): BatchRequestInterface;
+    public function send();
+}
