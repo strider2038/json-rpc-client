e7aa4af8d02c4af10f8dcb5ed4a04d5d2e5ee6c2
Igor Lazarev <strider2038@yandex.ru>
2019-03-16 09:14:32 +0300

    code style fix

--- src/Serialization/JsonObjectSerializer.php
+++ src/Serialization/JsonObjectSerializer.php
@@ -42,7 +42,7 @@ class JsonObjectSerializer implements MessageSerializerInterface
 
     public function deserialize(string $response)
     {
-        if (trim($response) === '') {
+        if ('' === trim($response)) {
             $result = null;
         } else {
             $decodedResponse = json_decode($response, false, $this->depth, $this->decodeOptions);
